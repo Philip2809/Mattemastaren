909ac63117214d15363c8997a98d102d55b600a7
Philip Magyar <philipmagyar@proton.me>
2025-09-12 19:26:58 +0200

    remove loading stuff

--- src/network/client.ts
+++ src/network/client.ts
@@ -1,10 +1,5 @@
 import axios from 'axios'
-declare module 'axios' {
-  export interface AxiosRequestConfig {
-    loadingString?: string;
-    loadingId?: string;
-  }
-}
+
 const httpClient = axios.create({
   baseURL: 'http://localhost:3000'
 })
