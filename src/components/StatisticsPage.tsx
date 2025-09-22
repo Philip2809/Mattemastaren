5586bb74e0efd51111770519bc8ac852b7e07a69
Philip Magyar <philipmagyar@proton.me>
2025-09-22 11:20:08 +0200

    use react-router

--- src/components/StatisticsPage.tsx
+++ src/components/StatisticsPage.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 import { exercises, categories, difficulties } from '../exercises';
 import './StatisticsPage.css';
 import { exercisesService } from '../fake-backend/exercises';
